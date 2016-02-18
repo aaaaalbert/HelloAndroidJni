ea1fd7941b5902bd8f99cae4d4a6b451175ca4e2
Albert (on sensibility-buildbox-32) <albert.rafetseder+github@univie.ac.at>
2016-02-18 16:41:07 +0100

    Enable logging for the C part
    You wouldn't believe from the size of this commit that this was
    quite difficult to work out :-D

--- app/src/main/jni/hello-android-jni.c
+++ app/src/main/jni/hello-android-jni.c
@@ -1,8 +1,8 @@
 #include <jni.h>
 #include <stdio.h>
-#include "/home/aaaaa/devel/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/log.h"
+#include <android/log.h>
 
-//# define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, __FILE__, __VA_ARGS__))
+# define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, __FILE__, __VA_ARGS__))
 
 JNIEXPORT jstring JNICALL
 Java_com_example_aaaaa_helloandroidjni_MainActivity_getMsgFromJni(JNIEnv *env,
@@ -23,7 +23,7 @@ Java_com_example_aaaaa_helloandroidjni_MainActivity_getMsgFromJni(JNIEnv *env,
 
 
 
-    //LOGI("Acceleration!!!! It's %f", anacceleration);
+    LOGI("Acceleration!!!! It's %f", anacceleration);
 
     sprintf(floatinstring, "%f", anacceleration);
     return (*env)->NewStringUTF(env, floatinstring);
